9d64f4a3bc44c037dc6ee2cf930088cccbd81975
bignya <146578548+bignya23@users.noreply.github.com>
2024-11-05 16:18:40 +0530

    chatbot added

---
+++ chatbot/collection_generator.py
@@ -0,0 +1,40 @@
+import PyPDF2
+import requests
+import json
+from dotenv import load_dotenv
+import os
+import json
+
+
+
+load_dotenv()
+VULTR_API_KEY = os.getenv("VULTR_API")
+collection_name = "sentify"
+
+
+VECTOR_STORE_ENDPOINT = "https://api.vultrinference.com/v1/vector_store"
+
+# Set up the headers
+headers = {
+    "Authorization": f"Bearer {VULTR_API_KEY}",
+    "Content-Type": "application/json"
+}
+
+
+payload = {
+    "name": collection_name  
+}
+
+
+response = requests.post(VECTOR_STORE_ENDPOINT, headers=headers, data=json.dumps(payload))
+
+
+if response.status_code == 201:  
+    print("Collection created successfully:", response.json())
+else:
+    print("Error creating collection:", response.status_code, response.text)
+
+
+
+
+
